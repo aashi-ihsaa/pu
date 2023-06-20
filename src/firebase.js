a6533594a60204549fe9ab770f66b1f803817f90
aashi-ihsaa <aashij.it.21@nitj.ac.in>
2023-06-20 16:46:47 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAzqDn4TMg9gEqVRFLRwK4sAM0l9gfmn5g",
+    authDomain: "netflix-165ea.firebaseapp.com",
+    projectId: "netflix-165ea",
+    storageBucket: "netflix-165ea.appspot.com",
+    messagingSenderId: "785471272321",
+    appId: "1:785471272321:web:9420f38bfa84c03706e480",
+    measurementId: "G-CYWHRV2CG3"
+  };
